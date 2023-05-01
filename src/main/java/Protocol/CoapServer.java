93d9021ee250c4c1eaf891b75ce3ffb07b7bf125
imperator0309 <respvblicaromana0309@gmail.com>
2023-05-01 18:09:07 +0700

    change to multithread-handling server

--- src/main/java/Protocol/CoapServer.java
+++ src/main/java/Protocol/CoapServer.java
@@ -31,7 +31,7 @@ public class CoapServer extends Thread {
                 serverSocket.receive(inPkt);
 
                 Endpoint endpoint = new Endpoint(inPkt.getAddress(), inPkt.getPort());
-                Exchange exchange = new Exchange(endpoint);
+                //Exchange exchange = new Exchange(endpoint);
 
                 buffer = new byte[inPkt.getLength()];
                 ByteArrayInputStream reader = new ByteArrayInputStream(inPkt.getData(), 0, inPkt.getLength());
@@ -39,7 +39,7 @@ public class CoapServer extends Thread {
 
                 Request request = new Request(buffer);
 
-                if (request == null) {
+                /*if (request == null) {
                     Response response = new Response();
                     response.setType(CoAP.Type.NON);
                     response.setTKL(0);
@@ -68,7 +68,11 @@ public class CoapServer extends Thread {
                         exchange.setResponse(new Response(request, CoAP.Type.NON));
                         resource.handleRequest(exchange);
                     }
-                }
+                }*/
+
+                HandleClient handleClient = new HandleClient(this.root, request, endpoint);
+                handleClient.start();
+
             } while (true);
         } catch (SocketException ex) {
             ex.printStackTrace();
@@ -119,5 +123,80 @@ public class CoapServer extends Thread {
 
     public RootResource getRoot() {
         return root;
+    }
+
+    private static class HandleClient extends Thread {
+        private Request request;
+        private Endpoint endpoint;
+        private RootResource root;
+        private HandleClient(RootResource root, Request request, Endpoint endpoint) {
+            this.root = root;
+            this.request = request;
+            this.endpoint = endpoint;
+        }
+
+        @Override
+        public void run() {
+            Exchange exchange = new Exchange(endpoint);
+
+            if (request == null) {
+                Response response = new Response();
+                response.setType(CoAP.Type.NON);
+                response.setTKL(0);
+                response.setMessageID(0);
+                response.setToken(null);
+                response.setOption(null);
+
+                exchange.setResponse(response);
+                exchange.respond(CoAP.ResponseCode.BAD_REQUEST, null);
+            } else if (request.getOption().getValue() == null) {
+                Response response = new Response(request, CoAP.Type.NON);
+                exchange.setResponse(response);
+                exchange.respond(CoAP.ResponseCode.BAD_OPTION, null);
+            } else {
+                exchange.setRequest(request);
+                String uri = new String(request.getOption().getValue(), 0,
+                        request.getOption().getOptionLength());
+
+                CoapResource resource = getResource(uri);
+
+                if (resource == null) {
+                    Response response = new Response(request, CoAP.Type.NON);
+                    exchange.setResponse(response);
+                    exchange.respond(CoAP.ResponseCode.NOT_FOUND, null);
+                } else {
+                    exchange.setResponse(new Response(request, CoAP.Type.NON));
+                    resource.handleRequest(exchange);
+                }
+            }
+        }
+
+        public CoapResource getResource(String url) {
+            try {
+                if (url.charAt(0) != '/')
+                    return null;
+                String[] name = url.substring(1).split("/");
+                Resource resource = getResource(this.root, name, 0);
+                return (CoapResource) resource;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        private Resource getResource(Resource resource ,String[] name, int index) {
+            try {
+                if (resource == null)
+                    return null;
+
+                if (index == name.length)
+                    return resource;
+
+                return getResource(resource.getChild(name[index]), name, index + 1);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 }
